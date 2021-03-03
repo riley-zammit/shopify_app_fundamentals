6fd4a8d07cfddb4d6950732a3cdcd1d50f5e87ef
R-J-Z <65351809+R-J-Z@users.noreply.github.com>
2021-03-03 11:18:03 -0500

    skeelteon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
+require 'shopify_app'
 class ApplicationController < ActionController::Base
+    before_action :setup_session
+    after_action :allow_iframe
+    private
+        def setup_session
+            ShopifyApp::Session.setup(api_key: Rails.configuration.api_key, secret: Rails.configuration.api_secret)
+        end
+
+        def allow_iframe
+            response.headers.except! 'X-Frame-Options'
+        end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,11 @@ module ShopifyAppFoundation
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    #-----CONFIG SHOPIFY APP------
+    config.api_key = ENV['SHOPIFY_API_KEY']
+    config.api_secret = ENV['SHOPIFY_API_SECRET']
+
+    config.app_root = "https://rjz.ngrok.io"
+    config.scope = "write_customers" #grants read/write
   end
 end
